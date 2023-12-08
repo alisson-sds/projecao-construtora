554bc7002d53d93245f304d999852659cc9fa428
Alisson <alisson-sds@hotmail.com>
2023-12-08 09:18:10 -0300

    Finalizando header

--- projecao/src/App.tsx
+++ projecao/src/App.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
+import Logo from "./assets/logo-proj.png";
+
 function App() {
+  
+  const [isMobileMenuOpenned, setIsMobileMenuOpenned] = useState<boolean>(false);
+
   return (
     <>
       <header>
         <nav>
-          <a className="logo" href="/">foto</a>
-          <div className="mobile-menu">
+          <a className="logo">
+            <img src={Logo} alt="Logo da empresa" title="Logo da empresa"></img>
+          </a>
+          <div className={`mobile-menu ${isMobileMenuOpenned? "active" : ""}`} onClick={() => setIsMobileMenuOpenned(!isMobileMenuOpenned)}>            
             <div className="line1"></div>
             <div className="line2"></div>
             <div className="line3"></div>
           </div>
-          <ul className="nav-list">
+          <ul className={`nav-list ${isMobileMenuOpenned? "active" : ""}`}>
             <li><a href="/">Início</a></li>
             <li><a href="/">Quem somos</a></li>
             <li><a href="/">Empreendimentos</a></li>
