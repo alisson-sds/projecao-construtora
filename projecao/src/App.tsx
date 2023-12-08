16f79c000a36473311d50bcae0f4d847d0b2ee7c
Alisson <alisson-sds@hotmail.com>
2023-12-07 22:11:02 -0300

    Iniciando projeto

---
+++ projecao/src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <a className="logo" href="/">foto</a>
+          <div className="mobile-menu">
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+          <ul className="nav-list">
+            <li><a href="/">Início</a></li>
+            <li><a href="/">Quem somos</a></li>
+            <li><a href="/">Empreendimentos</a></li>
+            <li><a href="/">Contato</a></li>
+            <li><a href="/">Localização</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+
+      </main>
+    </>
+  );
+}
+
+export default App;
