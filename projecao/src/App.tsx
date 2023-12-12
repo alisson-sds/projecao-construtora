941e1ffd7f447c3acb5ae2c60e40bc0ba65d1e24
Alisson <alisson-sds@hotmail.com>
2023-12-12 08:37:37 -0300

    Iniciando slider

--- projecao/src/App.tsx
+++ projecao/src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+
 import "./App.css";
 
 import Logo from "./assets/logo-proj.png";
 
 function App() {
-  
-  const [isMobileMenuOpenned, setIsMobileMenuOpenned] = useState<boolean>(false);
+  const [isMobileMenuOpenned, setIsMobileMenuOpenned] =
+    useState<boolean>(false);
 
   return (
     <>
@@ -14,26 +18,47 @@ function App() {
           <a className="logo">
             <img src={Logo} alt="Logo da empresa" title="Logo da empresa"></img>
           </a>
-          <div className={`mobile-menu ${isMobileMenuOpenned? "active" : ""}`} onClick={() => setIsMobileMenuOpenned(!isMobileMenuOpenned)}>            
+          <div
+            className={`mobile-menu ${isMobileMenuOpenned ? "active" : ""}`}
+            onClick={() => setIsMobileMenuOpenned(!isMobileMenuOpenned)}
+          >
             <div className="line1"></div>
             <div className="line2"></div>
             <div className="line3"></div>
           </div>
-          <ul className={`nav-list ${isMobileMenuOpenned? "active" : ""}`}>
-            <li><a href="/">Início</a></li>
-            <li><a href="/">Quem somos</a></li>
-            <li><a href="/">Empreendimentos</a></li>
-            <li><a href="/">Contato</a></li>
-            <li><a href="/">Localização</a></li>
+          <ul className={`nav-list ${isMobileMenuOpenned ? "active" : ""}`}>
+            <li>
+              <a href="/">Início</a>
+            </li>
+            <li>
+              <a href="/">Quem somos</a>
+            </li>
+            <li>
+              <a href="/">Empreendimentos</a>
+            </li>
+            <li>
+              <a href="/">Contato</a>
+            </li>
+            <li>
+              <a href="/">Localização</a>
+            </li>
           </ul>
         </nav>
-      </header>      
+      </header>
       <main>
-        {/*
-      <p>Parag</p>
-        <div className="img-roulette"></div>
-        */}
+        <Swiper>
+          <SwiperSlide>
+            <h1>teste</h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1>teste1</h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1>teste2</h1>
+          </SwiperSlide>
+        </Swiper>
       </main>
+      <footer></footer>
     </>
   );
 }
